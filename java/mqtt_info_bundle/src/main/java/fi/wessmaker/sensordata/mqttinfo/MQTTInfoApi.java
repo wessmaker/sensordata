fea554313cc1587408affa8fcc0cd3ea728b8c6c
Eemi Wessman <eemi.wessman@gmail.com>
2024-11-26 23:41:09 +0200

    style: Formatted files

--- java/mqtt_info_bundle/src/main/java/fi/wessmaker/sensordata/mqttinfo/MQTTInfoApi.java
+++ java/mqtt_info_bundle/src/main/java/fi/wessmaker/sensordata/mqttinfo/MQTTInfoApi.java
@@ -6,30 +6,32 @@ import javax.ws.rs.core.Response.*;
 import fi.wessmaker.sensordata.handler.MQTTHandler;
 
 public class MQTTInfoApi implements MQTTInfoService {
-
+	
 	@Override
-	public Response getAllInfo() {
-
+	public Response getAllInfo () {
 		return Response.status(Status.OK).entity(MQTTHandler.get().getMQTTInfo()).build();
 	}
-
+	
 	@Override
-	public Response getTopic(String path) {
+	public Response getTopic (String path) {
 		return Response.status(Status.OK).entity(MQTTHandler.get().getCustomTopic(path)).build();
 	}
-
+	
 	@Override
-	public Response getAllTopics() {
-		return Response.status(Status.OK).entity(MQTTHandler.get().getMQTTInfo().getCustomTopics()).build();
+	public Response getAllTopics () {
+		return Response.status(Status.OK).entity(MQTTHandler.get().getMQTTInfo().getCustomTopics())
+				.build();
 	}
-
+	
 	@Override
-	public Response getTopicCount() {
-		return Response.status(Status.OK).entity(MQTTHandler.get().getMQTTInfo().getCustomTopics().size()).build();
+	public Response getTopicCount () {
+		return Response.status(Status.OK)
+				.entity(MQTTHandler.get().getMQTTInfo().getCustomTopics().size()).build();
 	}
-
+	
 	@Override
-	public Response getConnection() {
-		return Response.status(Status.OK).entity(MQTTHandler.get().getMQTTInfo().isConnection()).build();
+	public Response getConnection () {
+		return Response.status(Status.OK).entity(MQTTHandler.get().getMQTTInfo().isConnection())
+				.build();
 	}
 }
