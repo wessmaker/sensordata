610e96536fc44b703d1196923796c1a2a544f63c
Eemi Wessman <eemi.wessman@gmail.com>
2024-11-28 17:46:07 +0200

    refactor: Added namespaces

---
+++ cpp/t_display_mqtt_tester/src/display/display.h
@@ -0,0 +1,12 @@
+#pragma once 
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+namespace Display{
+   
+}
+
+
+
+
+#endif
---
+++ cpp/t_display_mqtt_tester/src/sensors/light_sensor.h
@@ -0,0 +1,12 @@
+#pragma once 
+#ifndef LIGHT_SENSOR_H
+#define LIGHT_SENSOR_H
+
+namespace LightSensor{
+   
+}
+
+
+
+
+#endif
