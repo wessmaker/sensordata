f0547898f5f2dbf4061659ad1aa698e9d4871ca9
Eemi Wessman <eemi.wessman@gmail.com>
2024-12-03 23:50:09 +0200

    refactor: Refactored variables and function positions of buttons.cpp

--- cpp/t_display_mqtt_tester/src/sensors/Buttons.cpp
+++ cpp/t_display_mqtt_tester/src/sensors/Buttons.cpp
@@ -6,40 +6,40 @@
 #include "communication/debug.h"
 
 
-Button2 leftButton;
-Button2 rightButton;
-Button2 extButton1;
-u_long timer = 0;
-u_int16_t startPressTime = 1000;
-u_int16_t stopPressTime = 1500;
-bool runAfter = false;
 #define EXT_BUTTON 37
 
 
-void _noneHandler(Button2& b){};
+Button2 leftButton;
+Button2 rightButton;
+Button2 extButton1;
+
+bool runOnRelease = false;
+
+u_long timer = 0;
+u_int16_t timeToStop = 1000;  // 1s
+u_int16_t timeToStart = 1500; // 1.5s
 
 
-void leftButtonClick(Button2& b){
+void _emptyHandler(Button2& b){};
+
+void buttonClickLeft(Button2& b){
    Debugging::debug("Left button tap");
    UI::menuMove(UI::DOWN);
 }
 
-
-void rightButtonClick(Button2& b){
+void buttonClickRight(Button2& b){
    Debugging::debug("Right button tap");
    UI::menuMove(UI::UP);
 }
 
-
-void extButton1Click(Button2& b){
+void buttonClickExt1(Button2& b){
    Debugging::debug("External button tap");
 }
 
-
 void removeHandlers(){
-   leftButton.setTapHandler(_noneHandler);
-   rightButton.setTapHandler(_noneHandler);
-   extButton1.setTapHandler(_noneHandler);
+   leftButton.setTapHandler(_emptyHandler);
+   rightButton.setTapHandler(_emptyHandler);
+   extButton1.setTapHandler(_emptyHandler);
 }
 
 // Handles double press of 2 onboard buttons
@@ -48,16 +48,16 @@ void handleDoublePress(){
    if (leftButton.isPressed() && rightButton.isPressed() && Controller::isRunning())
    {
       if (!timer) timer = millis();  
-      if (millis() - timer >= stopPressTime) 
+      if (millis() - timer >= timeToStart) 
       {
-         runAfter = true;
+         runOnRelease = true;
          removeHandlers();
          UI::setState(UI::STOPPING);
       }
    }
-   else if (!(leftButton.isPressed() || rightButton.isPressed()) && runAfter)   // Stopping after releasing both buttons
+   else if (!(leftButton.isPressed() || rightButton.isPressed()) && runOnRelease)   // Stopping after releasing both buttons
    {
-      runAfter = false;
+      runOnRelease = false;
       timer = 0;
       Controller::stop();
    }
@@ -65,19 +65,16 @@ void handleDoublePress(){
 }
 
 
-
 namespace Buttons{
-
 
    void init(){
       leftButton.begin(LEFT_BUTTON);
       rightButton.begin(RIGHT_BUTTON);
       extButton1.begin(EXT_BUTTON);
-      leftButton.setTapHandler(leftButtonClick);
-      rightButton.setTapHandler(rightButtonClick);
-      extButton1.setTapHandler(extButton1Click);
+      leftButton.setTapHandler(buttonClickLeft);
+      rightButton.setTapHandler(buttonClickRight);
+      extButton1.setTapHandler(buttonClickExt1);
    }
-
 
    void loop(){
       leftButton.loop();  
@@ -85,10 +82,6 @@ namespace Buttons{
       extButton1.loop();
       handleDoublePress();
    }
-
-
-
-
 
    /**
     * Only handle pressing of both buttons at the same time for given time
@@ -100,20 +93,18 @@ namespace Buttons{
       if (leftButton.isPressed() && rightButton.isPressed() && !Controller::isRunning())
       {
          if (!timer) timer = millis();  
-         if (millis() - timer >= startPressTime)
+         if (millis() - timer >= timeToStop)
          {
-            runAfter = true;
+            runOnRelease = true;
             UI::setState(UI::STARTING);
          }
       }
-      else if (!(leftButton.isPressed() || rightButton.isPressed()) && runAfter)   // Starting after releasing both buttons
+      else if (!(leftButton.isPressed() || rightButton.isPressed()) && runOnRelease)   // Starting after releasing both buttons
       {
-         runAfter = false;
+         runOnRelease = false;
          timer = 0;
          Controller::start();
       }
       else if (timer) timer = 0; // Reset timer
    }
-
-
 }
